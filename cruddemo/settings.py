094d72727b13b92c48e38e428685b9111f43a111
Jithin Jose <007jithinjose@gmail.com>
2024-01-15 15:18:06 +0530

    backend test1

--- cruddemo/settings.py
+++ cruddemo/settings.py
@@ -1,5 +1,10 @@
 
+import os
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,12 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c&iy$!d_33#ty02kjs*7ol4jg+ypoelvk7dtz6@we)b!3jr-9f'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -65,10 +70,7 @@ WSGI_APPLICATION = 'cruddemo.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
 
 
